3e3fd04b26820bb9d46007be38d1669ae76e9163
stelaninja <stefan@johansson.com>
2022-12-04 15:18:00 +0100

    Completed day 4

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Advent of Code 2022 in Python
 * Day 1 - Completed
 * Day 2 - Completed
 * Day 3 - Completed
+* Day 4 - Completed
 * Day 4 - Not started
--- day4.py
+++ day4.py
@@ -16,11 +16,27 @@ response = requests.get(
     f"https://adventofcode.com/2022/day/{DAY}/input", cookies=SESSION_KEY, timeout=500
 )
 
-data = response.text
+data = response.text.strip().split("\n")
+assignments = [x.split(",") for x in data]
 
-data = """
-"""
 
-data = data.strip().split("\n")
+contained = 0
+overlap = 0
 
-print(data)
+for e1, e2 in assignments:
+    e1 = [int(x) for x in e1.split("-")]
+    e2 = [int(x) for x in e2.split("-")]
+
+    s1 = set(range(e1[0], e1[1] + 1))
+    s2 = set(range(e2[0], e2[1] + 1))
+
+    # PART 1
+    if s1 <= s2 or s2 <= s1:
+        contained += 1
+
+    # PART 2
+    if s1 & s2:
+        overlap += 1
+
+print(f"Part 1: {contained}")
+print(f"Part 2: {overlap}")
