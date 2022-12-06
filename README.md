5368e2279e2d3ae5da45589e5a198791cdff62f3
stelaninja <stefan@johansson.com>
2022-12-06 16:29:37 +0100

    Completed day 6

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # AdventOfCode2022
-Advent of Code 2022 in Python
+My solutions to Advent of Code 2022, in Python.
 
 * Day 1 - Completed
 * Day 2 - Completed
 * Day 3 - Completed
 * Day 4 - Completed
 * Day 5 - Completed (spent many hours thinking I could maximum move 3 crates per move)
-* Day 6 - Not started
+* Day 6 - Completed
+* Day 7 - Not started
---
+++ day6.py
@@ -0,0 +1,36 @@
+"""
+Day 6 of Advent Of Code 2022
+https://adventofcode.com/2022/
+"""
+import os
+import re
+import sys
+
+import requests
+
+SESSION_KEY = {"session": os.environ.get("SESSION_KEY", None)}
+DAY = int(re.findall(r"[0-9]", sys.argv[0].rsplit("/", maxsplit=1)[-1])[0])
+
+
+response = requests.get(
+    f"https://adventofcode.com/2022/day/{DAY}/input", cookies=SESSION_KEY, timeout=500
+)
+
+data = response.text.strip()
+
+marker_length = {
+    "packet": 4,
+    "message": 14,
+}
+
+for part, marker_type in zip(range(1, 3), ["packet", "message"]):
+    marker = False
+    m_len = marker_length[marker_type]
+    idx = m_len - 1
+
+    while not marker:
+        idx += 1
+        if len(set(data[idx-m_len:idx])) == m_len:
+            marker = True
+
+    print(f"Part {part}: {idx}") 
